14fa0943567b54de4480db9aec2ed708a1e39766
ryansldev <ryanlima1309@gmail.com>
2022-03-05 19:42:25 -0300

    :sparkles: Connect mongoose

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+
 import { router } from "./routes";
 
 const app = express();
+
+if(process.env.MONGODB_CONNECTION_URL) {
+  mongoose.connect(process.env.MONGODB_CONNECTION_URL);
+}
+
+app.use(express.json());
 app.use(router);
 
 export { app };
