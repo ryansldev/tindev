dfa153cd5ec3afd42b943581acae1437ca64eeee
ryansldev <ryanlima1309@gmail.com>
2022-03-10 19:11:50 -0300

    :sparkles: Login page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,14 +1,35 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import styles from '../styles/Login.module.css';
 
 const Home: NextPage = () => {
+  const [username, setUsername] = useState('');
+  const router = useRouter();
+
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    router.push('/main');
+  }
+  
   return (
     <div>
       <Head>
         <title>Tindev</title>
       </Head>
 
-      <main>
+      <main className={styles.loginContainer}>
+        <form onSubmit={handleSubmit}>
+          <Image src='/logo.svg' alt='tindev' height='80px' width='80px' />
+          <input
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            placeholder='Digite seu usuário no GitHub'
+          />
+          <button type="submit">Enviar</button>
+        </form>
       </main>
     </div>
   )
