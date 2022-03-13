645c9a09fbfb2e4158999a6a97b6bca1ba7ecea4
ryansldev <ryanlima1309@gmail.com>
2022-03-12 23:53:58 -0300

    :sparkles: Add empty list of developers warn on main page

--- frontend/pages/dev/[id].tsx
+++ frontend/pages/dev/[id].tsx
@@ -1,10 +1,10 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import { api } from '../services/api';
-import styles from './index.module.css';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import styles from "./index.module.css";
 
 interface Dev {
   _id: string;
@@ -23,17 +23,17 @@ const Main: NextPage = () => {
   const [devs, setDevs] = useState<Dev[]>([]);
 
   const loadDevs = async () => {
-    const { data } = await api.get('/devs', {
+    const { data } = await api.get("/devs", {
       headers: {
-        user: `${id}`
-      }
+        user: `${id}`,
+      },
     });
 
     setDevs(data);
-  }
+  };
 
   useEffect(() => {
-    if(!id) {
+    if (!id) {
       return;
     }
 
@@ -43,22 +43,22 @@ const Main: NextPage = () => {
   const handleDislike = async (targetDevId: string) => {
     await api.post(`/devs/${targetDevId}/dislikes`, {
       headers: {
-        user: id
-      }
+        user: id,
+      },
     });
 
-    setDevs(devs.filter(dev => dev._id !== targetDevId));
-  }
+    setDevs(devs.filter((dev) => dev._id !== targetDevId));
+  };
 
   const handleLike = async (targetDevId: string) => {
     await api.post(`/devs/${targetDevId}/likes`, null, {
       headers: {
-        user: `${id}`
-      }
+        user: `${id}`,
+      },
     });
 
-    setDevs(devs.filter(dev => dev._id !== targetDevId));
-  }
+    setDevs(devs.filter((dev) => dev._id !== targetDevId));
+  };
 
   return (
     <div>
@@ -67,35 +67,54 @@ const Main: NextPage = () => {
       </Head>
 
       <main className={styles.mainContainer}>
-        <Image src='/logo.svg' alt='tindev' width='100' height='100' />
-        { devs.length > 0 &&
+        <Image src="/logo.svg" alt="tindev" width="100" height="100" />
+        {devs.length > 0 ? (
           <ul>
-            { devs.map((dev) => {
+            {devs.map((dev) => {
               return (
                 <li key={dev._id}>
-                  <img src={dev.avatar} alt={dev.name}/>
+                  <img src={dev.avatar} alt={dev.name} />
                   <footer>
                     <strong>{dev.name}</strong>
                     <p>{dev.bio}</p>
                   </footer>
 
                   <div className={styles.buttons}>
-                    <button type='button' onClick={() => handleDislike(dev._id)}>
-                      <div><Image src='/dislike.svg' alt='Dislike' width='20' height='20' /></div>
+                    <button
+                      type="button"
+                      onClick={() => handleDislike(dev._id)}
+                    >
+                      <div>
+                        <Image
+                          src="/dislike.svg"
+                          alt="Dislike"
+                          width="20"
+                          height="20"
+                        />
+                      </div>
                     </button>
 
-                    <button type='button' onClick={() => handleLike(dev._id)}>
-                      <div><Image src='/like.svg' alt='Like' width='20' height='20' /></div>
+                    <button type="button" onClick={() => handleLike(dev._id)}>
+                      <div>
+                        <Image
+                          src="/like.svg"
+                          alt="Like"
+                          width="20"
+                          height="20"
+                        />
+                      </div>
                     </button>
                   </div>
                 </li>
-              )
+              );
             })}
           </ul>
-        }
+        ) : (
+          <div className={styles.empty}>Acabou :(</div>
+        )}
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
